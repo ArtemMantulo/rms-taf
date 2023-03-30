7dfb2cb81d40829f56ef6f02cbddb767e8c89931
ArtemMantulo <57362577+ArtemMantulo@users.noreply.github.com>
2023-03-31 00:41:30 +0300

    Update GetProductsTest.java

--- src/test/java/api/GetProductsTest.java
+++ src/test/java/api/GetProductsTest.java
@@ -15,27 +15,9 @@ import static framework.SchemaTemplates.SCHEMA_TEMPLATE_GET_PRODUCTS;
 import static io.restassured.RestAssured.given;
 
 public class GetProductsTest extends BaseTest {
-
-    public static final String PRODUCTS = "/products";
-
     @Severity(SeverityLevel.CRITICAL)
     @Test(description = "Get list of available products", groups = {"Smoke", "api"})
-    public void getProductsTest() {
-        RestAssured.baseURI = ENV_URL.getValue() + "/api";
-
-        ValidatableResponse response =
-                given().auth()
-                        .basic(MAIN_USER.getName(), MAIN_USER_PASSWORD.getName())
-                        .queryParam("supplierName", "all")
-                        .queryParam("status", "all")
-                        .queryParam("nonCompletedTasks", "all")
-                        .when()
-                        .get(PRODUCTS)
-                        .then()
-                        .assertThat().contentType(ContentType.HTML);
-        validateResponse(response, SCHEMA_TEMPLATE_GET_PRODUCTS);
-
-        int log = response.log().body().extract().statusCode();
-        Allure.addAttachment("Console log: ", String.valueOf(log));
+    public void executeMytest() {
+      System.out.println("My test");
     }
 }
